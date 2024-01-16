b73fea5de9b503b8815c7e94fa8ebf4534b36b05
ddeflores <94253526+ddeflores@users.noreply.github.com>
2024-01-15 22:58:44 -0500

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -6,16 +6,6 @@ import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBf7QpxfQIc-UU-famBE47hNTXBkYccs8M",
-  authDomain: "studybuddy-a980f.firebaseapp.com",
-  projectId: "studybuddy-a980f",
-  storageBucket: "studybuddy-a980f.appspot.com",
-  messagingSenderId: "107501556428",
-  appId: "1:107501556428:web:ba055d65f79d717032e869",
-  measurementId: "G-XE98Y5BKDL",
-  databaseURL: "https://studybuddy-a980f-default-rtdb.firebaseio.com"
-};
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
